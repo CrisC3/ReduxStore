7d4fe0fb2458c2114d7c7224e9f70129d846ec60
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-08-02 13:50:39 -0500

    Created actions folder and move the actions.js file into the folder

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,7 +4,7 @@ import { useQuery } from "@apollo/client";
 
 import Cart from "../components/Cart";
 import { useStoreContext } from "../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from "../utils/actions";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from "../actions/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
 import spinner from "../assets/spinner.gif";

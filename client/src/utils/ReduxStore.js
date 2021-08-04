1ae75d47a9b881cebb2b7c5d8a6a62934ba0d2d8
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-08-04 09:19:41 -0500

    Added debug reference for redux

--- client/src/utils/ReduxStore.js
+++ client/src/utils/ReduxStore.js
@@ -1,4 +1,9 @@
 import { createStore } from "redux";
 import reducers from "./reducers";
 
-export default createStore(reducers);
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// Added for showing in F12 debug tools
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
